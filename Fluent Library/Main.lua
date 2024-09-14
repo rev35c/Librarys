662744f5d1f5d45f31ced49b18f97ae484af8546
Skinny-yz <luizfelipe.88jka@gmail.com>
2024-09-13 23:14:58 -0300

    Update Main.lua

--- Fluent Library/Main.lua
+++ Fluent Library/Main.lua
@@ -1,3 +1,5 @@
 
 
     game.Workspace.TPSSystem.TPS.Size = Vector3.new(10, 10, 10)
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = 2346798698732678432678456823
+ game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 9764879326879436278456
